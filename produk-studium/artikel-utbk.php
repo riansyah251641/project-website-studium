dae22094da68962d5207a73d28a542fc2ffe7f4f
riansyah251641 <91471268+riansyah251641@users.noreply.github.com>
2023-04-10 14:47:43 +0700

    Add files via upload

---
+++ produk-studium/artikel-utbk.php
@@ -0,0 +1,265 @@
+<html>
+
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=windows-1252">
+<meta name=Generator content="Microsoft Word 15 (filtered)">
+<style>
+<!--
+ /* Font Definitions */
+ @font-face
+	{font-family:"Cambria Math";
+	panose-1:2 4 5 3 5 4 6 3 2 4;}
+@font-face
+	{font-family:Calibri;
+	panose-1:2 15 5 2 2 2 4 3 2 4;}
+@font-face
+	{font-family:Roboto;}
+ /* Style Definitions */
+ p.MsoNormal, li.MsoNormal, div.MsoNormal
+	{margin-top:0cm;
+	margin-right:0cm;
+	margin-bottom:8.0pt;
+	margin-left:0cm;
+	line-height:107%;
+	font-size:11.0pt;
+	font-family:"Calibri",sans-serif;}
+.MsoChpDefault
+	{font-family:"Calibri",sans-serif;}
+.MsoPapDefault
+	{margin-bottom:8.0pt;
+	line-height:107%;}
+@page WordSection1
+	{size:612.0pt 792.0pt;
+	margin:72.0pt 72.0pt 72.0pt 72.0pt;}
+div.WordSection1
+	{page:WordSection1;}
+-->
+</style>
+
+</head>
+
+<body lang=EN-US link=blue vlink="#954F72" style='word-wrap:break-word'>
+
+<div class=WordSection1>
+
+<p class=MsoNormal align=center style='margin-top:22.5pt;margin-right:0cm;
+margin-bottom:22.5pt;margin-left:0cm;text-align:center;line-height:normal;
+background:white'><b><span style='font-size:26.0pt;font-family:Roboto;
+color:black;letter-spacing:-.4pt'>Apa itu UTBK ? ,Pengertian,Latar Belakang ,
+dan Tujuan</span></b></p>
+
+<p class=MsoNormal style='margin-top:22.5pt;margin-right:0cm;margin-bottom:
+22.5pt;margin-left:0cm;line-height:normal;background:white'><b><span
+style='font-size:18.0pt;font-family:Roboto;color:black;letter-spacing:-.4pt'>Pengertian
+UTBK</span></b></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Ujian
+tulis berbasis komputer&nbsp;belum merupakan cara seleksi untuk mengetahui
+kemampuan akademik dari calon mahasiswa melalui sistem komputer. Adanya
+penggunaan basis komputer dalam ujian ini dibuat berdasarkan ketentuan terkini
+sebagai kesetaraan dengan teknologi yang berjalan.</span></p>
+
+<p class=MsoNormal align=center style='margin-bottom:0cm;text-align:center;
+line-height:normal'><a
+href="https://i0.wp.com/www.zenius.net/blog/wp-content/uploads/2021/05/Pengertiaan-Latar-Belakang-Tujuan-dan-Ketentuan-Umum-UTBK.jpg?ssl=1"><span
+style='font-size:12.0pt;font-family:"Times New Roman",serif;text-decoration:
+none'><img border=0 width=887 height=499 id="Picture 1"
+src="artikel-utbk_files/image001.jpg"
+alt="Pengertiaan, Latar Belakang, Tujuan dan Ketentuan Umum UTBK"></span></a></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Hal ini
+tentu mau menghapus metode lama yakni ujian tertulis yang lebih memakan banyak
+waktu dan tenaga serta memiliki tingkat akurasi yang lebih rendah. Ini menuntut
+para peserta untuk lebih giat mempersiapkan diri dengan berbagai metode dan
+strategi pembelajaran atau latihan.</span></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Mengingat
+masuk pada sebuah PTN menjadi mimpi besar bagi para pemuda pemudi bangsa. Hal
+ini menjadi salah satu bagian penting dalam perjuangan menggapai impian dan
+cita-cita serta masa depan yang lebih cerah dan menjanjikan.</span></p>
+
+<p class=MsoNormal style='margin-top:22.5pt;margin-right:0cm;margin-bottom:
+22.5pt;margin-left:0cm;line-height:normal;background:white'><b><span
+style='font-size:18.0pt;font-family:Roboto;color:black;letter-spacing:-.4pt'>Latar
+Belakang UTBK</span></b></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Mari
+menilik lebih dulu latar belakang dari diadakannya&nbsp;ujian tulis berbasis
+komputer&nbsp;telah berlangsung sejak 2019 sebagai pengiringan terhadap SBMPTN
+atau Seleksi Bersama Masuk Perguruan Tinggi Negeri. Kriteria ini disebabkan
+oleh PTN untuk meng-upgrade metode digital dengan penggunaan sistem komputer.</span></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Ujian
+tulis ini dilangsungkan oleh&nbsp;Lembaga tes masuk perguruian tinggi ada yang
+disingkat menjadi LTMPT. Lembaga ini pun menjadi satu-satunya yang melakukan
+penyelenggaraan sekaligus tes perguruan tinggi negeri di Indonesia.</span></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Dengan
+adanya pihak tersebut maka hasil ujian memiliki kredibilitas, sesuai standar
+serta akurasi nilai peserta. Adapun untuk ujian tulis berbasis komputer yang
+diadakan pada tahun 2021 bisa diikuti oleh seluruh alumni tingkat SMA dan SMK
+2019, 2020 dan 2021.</span></p>
+
+<p class=MsoNormal style='margin-top:22.5pt;margin-right:0cm;margin-bottom:
+22.5pt;margin-left:0cm;line-height:normal;background:white'><b><span
+style='font-size:18.0pt;font-family:Roboto;color:black;letter-spacing:-.4pt'>Tujuan
+UTBK</span></b></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Tujuan dari
+ujian tertulis dengan basis komputer ini memiliki penggabungan dengan tujuan
+dari terselenggaranya seleksi masuk perguruan tinggi. Keduanya memiliki
+keterkaitan dalam suatu proses penyaringan peserta yang paling layak. Adapun
+tujuan tersebut adalah sebagai berikut.</span></p>
+
+<p class=MsoNormal style='margin-top:22.5pt;margin-right:0cm;margin-bottom:
+22.5pt;margin-left:0cm;line-height:normal;background:white'><b><span
+style='font-size:13.5pt;font-family:Roboto;color:black;letter-spacing:-.4pt'>1.
+Mampu Memprediksi Dengan Lebih Baik dan Tepat</span></b></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Dengan
+dilakukannya ujian tertulis maka pihak PTN dan lembaga dapat memprediksi calon
+mahasiswa dapat yang mampu untuk melalui masa studi dengan baik serta selesai
+pada waktu yang tepat. Tujuan ini tidak hanya bermanfaat bagi kelangsungan
+prosedur pada perguruan tinggi saja.</span></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Namun
+juga memastikan bahwa calon mahasiswa yang dapat memperhitungkan
+langkah-langkah dan rencananya di kemudian hari. Dengan demikian hal ini juga
+berhubungan dengan planning yang terarah dan terukur serta mampu memajukan
+kualitas anak bangsa.</span></p>
+
+<p class=MsoNormal style='margin-top:22.5pt;margin-right:0cm;margin-bottom:
+22.5pt;margin-left:0cm;line-height:normal;background:white'><b><span
+style='font-size:13.5pt;font-family:Roboto;color:black;letter-spacing:-.4pt'>2.
+Memberikan Tes Secara Fleksibel</span></b></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Dengan
+bantuan basis online yang yang diperbarui, maka hal ini akan memberikan
+kemudahan bagi para calon mahasiswa untuk mengikuti ujian tertulis secara lebih
+fleksibel. Dalam artian penentuan waktu ujian bisa dilakukan berdasarkan
+ketersediaan beserta, serta lokasi yang juga lebih fleksibel.</span></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Peserta
+dapat memilih lokasi ujian sesuai dengan lingkungan terdekat. Hal ini akan
+berpengaruh pada waktu yang lebih efisien serta pembagian kerja lembaga
+sekaligus pelaksanaan oleh peserta yang lebih maksimal tanpa adanya pengeluaran
+biaya dan transport khusus.</span></p>
+
+<p class=MsoNormal style='margin-top:22.5pt;margin-right:0cm;margin-bottom:
+22.5pt;margin-left:0cm;line-height:normal;background:white'><b><span
+style='font-size:13.5pt;font-family:Roboto;color:black;letter-spacing:-.4pt'>3.
+Memberi Kesempatan Bagi Calon Mahasiswa Untuk Memilih PTN Dengan Lintas Wilayah</span></b></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Tujuan
+berikutnya diambil berdasarkan SBMPTN yakni adanya kesempatan bagi calon mahasiswa
+untuk menentukan sendiri pilihan PTN. Bahkan dapat memilih berbagai PTN dengan
+lintas wilayah yakni berbeda dengan lokasi tempat tinggal atau domisilinya.</span></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Tak
+sedikit para mahasiswa yang merantau atau menempuh studi di kota dan Wilayah
+lain di Indonesia. Hal tersebut merupakan perwujudan dari adanya SBMPTN yang
+juga melalui ujian tertulis berbasis komputer sehingga apa yang dicita-citakan
+terwujud.</span></p>
+
+<p class=MsoNormal style='margin-top:22.5pt;margin-right:0cm;margin-bottom:
+22.5pt;margin-left:0cm;line-height:normal;background:white'><b><span
+style='font-size:13.5pt;font-family:Roboto;color:black;letter-spacing:-.4pt'>4.
+Membantu Menyeleksi Calon Mahasiswa Berdasarkan Kemampuan Akademik</span></b></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Tujuan
+lainnya adalah membantu menyeleksi calon mahasiswa berdasarkan kemampuan
+akademik. Hal ini akan menjadi keuntungan serta dapat membantu pihak PTN guna
+melakukan pemetaan terhadap siswa berpotensi sekaligus masuk pada kriteria yang
+diharapkan.</span></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Seleksi
+ini juga dapat mengukur kemampuan calon mahasiswa itu sendiri terhadap materi
+akademik yang dikuasai. Dengan demikian calon Mahasiswa juga dituntut untuk
+mempersiapkan diri secara lebih matang guna menghadapi ujian tertulis. Hal ini
+dimaksudkan biar yang bakal diperoleh dapat maksimal.</span></p>
+
+<p class=MsoNormal style='margin-top:22.5pt;margin-right:0cm;margin-bottom:
+22.5pt;margin-left:0cm;line-height:normal;background:white'><b><span
+style='font-size:18.0pt;font-family:Roboto;color:black;letter-spacing:-.4pt'>Ketentuan
+Umum UTBK</span></b></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Setelah
+mengetahui pengertian latar belakang dan tujuan maka penting untuk memahami
+beberapa ketentuan umum yang berlaku. Sebagai ujian seleksi Maka terdapat
+beberapa poin yang harus diperhatikan oleh seluruh calon mahasiswa atau
+peserta. Diantara ketentuan umum tersebut adalah sebagai berikut.</span></p>
+
+<p class=MsoNormal style='margin-top:22.5pt;margin-right:0cm;margin-bottom:
+22.5pt;margin-left:0cm;line-height:normal;background:white'><b><span
+style='font-size:13.5pt;font-family:Roboto;color:black;letter-spacing:-.4pt'>1.
+Mengikuti Satu Kali Ujian Pada Satu Periode</span></b></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Penting
+untuk Diingat dan diperhatikan bahwa ujian tertulis tersebut hanya dapat
+diikuti satu kali oleh setiap calon mahasiswa berdasarkan periodenya. Dalam
+permisalan pada tahun 2021 seseorang hanya mampu mengikuti satu kali ujian.</span></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Dengan
+demikian apabila calon peserta tidak lulus maka dapat mencoba lagi di tahun
+berikutnya. Hal ini memiliki dampak positif yakni kesiapan mental dan
+penguasaan materi yang dapat lebih dipersiapkan kembali secara lebih baik, atau
+membuka kemungkinan rencana lainnya.</span></p>
+
+<p class=MsoNormal style='margin-top:22.5pt;margin-right:0cm;margin-bottom:
+22.5pt;margin-left:0cm;line-height:normal;background:white'><b><span
+style='font-size:13.5pt;font-family:Roboto;color:black;letter-spacing:-.4pt'>2.
+Hasil Ujian Hanya Berlaku Untuk Satu Seleksi dan Penerimaan</span></b></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Hasil
+ujian yang telah dilalui oleh calon mahasiswa hanya berlaku untuk satu periode
+SBMPTN sekaligus satu keputusan pada penerimaan di PTN. Dengan demikian kamu
+harus mempersiapkan kiri apabila lolos dalam seleksi sehingga masuk pada
+perguruan tinggi yang telah dipilih.<br>
+Dengan demikian jika kamu melewatkan kesempatan yang telah terbuka maka
+penawaran tersebut tidak akan datang kembali hingga batas waktu yang
+ditentukan. Oleh sebab itu peluang ini harus dimanfaatkan semaksimal mungkin.</span></p>
+
+<p class=MsoNormal style='margin-top:22.5pt;margin-right:0cm;margin-bottom:
+22.5pt;margin-left:0cm;line-height:normal;background:white'><b><span
+style='font-size:13.5pt;font-family:Roboto;color:black;letter-spacing:-.4pt'>3.
+SBMPTN Merupakan Hasil Ujian Tertulis dan Kriteria Khusus dari PTN</span></b></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Hasil
+keputusan yang dikeluarkan SBMPTN&nbsp;merupakan buah dari bersedekah dan
+kriteria lain berdasarkan ketentuan perguruan tinggi negeri. Dengan demikian
+kamu bisa tahu bahwa hasil akhir yang diterima merupakan nilai kalkulasi atau
+penggabungan dari dua unsur.</span></p>
+
+<p class=MsoNormal style='margin-bottom:18.75pt;line-height:normal;background:
+white'><span style='font-size:14.5pt;font-family:Roboto;color:#45464B'>Oleh
+sebab itu penting untuk bagaimana prosedur dan ketentuan yang berlaku pada PTN
+tujuan kamu. Hal ini dilakukan agar tingkat kelolosan dapat lebih tinggi dan
+memenuhi harapan.</span></p>
+
+<p class=MsoNormal>&nbsp;</p>
+
+</div>
+
+</body>
+
+</html>
